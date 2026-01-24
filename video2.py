e62eca249b7cdd61348c0bf920c4ad08cb3ecafe
Vera Gonzalez <me@vera.lgbt>
2026-01-24 00:03:31 -0500

    experimenting w color

--- video2.py
+++ video2.py
@@ -6,6 +6,7 @@ from typing import List, LiteralString, Optional
 from indic_transliteration import sanscript
 from indic_transliteration.sanscript import transliterate
 from janim.imports import (
+    BLUE,
     DOWN,
     GREEN,
     # LEFT,
@@ -14,6 +15,7 @@ from janim.imports import (
     PURE_GREEN,
     # RIGHT,
     UP,
+    WHITE,
     Aligned,
     AnimGroup,
     Color,
@@ -38,14 +40,16 @@ SCALE = 2.0
 class Writing:
     devanagari: LiteralString
     iast: LiteralString
+    color: str
 
-    def __init__(self, itrans):
+    def __init__(self, itrans: LiteralString, color: str = WHITE):
         self.devanagari = transliterate(itrans, sanscript.ITRANS, sanscript.DEVANAGARI)
         self.iast = transliterate(itrans, sanscript.ITRANS, sanscript.IAST)
+        self.color = color
 
     def render(self, direction=DOWN):
-        devanagari = Jaini(self.devanagari)
-        iast = Jaini(self.iast)
+        devanagari = Jaini(self.devanagari, self.color)
+        iast = Jaini(self.iast, self.color)
         group = Group(devanagari)
         group.points.arrange(direction)
         return group
@@ -74,12 +78,13 @@ class Node:
 
     def __init__(
         self,
-        before: LiteralString,
+        before: Writing,
         children: Optional[Children] = None,
         after: Optional[LiteralString] = None,
     ):
-        self.before = Writing(before)
+        self.before = before
         self.children = children
+
         if after is None:
             self.after = after
             self.ag = None
@@ -212,10 +217,12 @@ class Sloka:
 class Word:
     before: LiteralString
     after: Optional[LiteralString]
+    color: str
 
-    def __init__(self, before, after=None):
+    def __init__(self, before, after=None, color=WHITE):
         self.before = before
         self.after = after
+        self.color = color
 
     def safe_after(self):
         if self.after is None:
@@ -224,20 +231,34 @@ class Word:
             return self.after
 
 
-def Jaini(text: LiteralString, scale: Optional[float] = SCALE):
-    return TypstText('#text(font: "Jaini", stroke: none)[%s]' % text, scale=scale)
+def Jaini(text: LiteralString, color: str = WHITE, scale: Optional[float] = SCALE):
+    return TypstText(
+        f'#text(font: "Jaini", stroke: none, fill: rgb("{color}"))[{text}]', scale=scale
+    )
+
+    # c = a == true then 5 else 4
 
 
-# c = a == true then 5 else 4
+def aft(a, b):
+    if a == b:
+        return None
+    else:
+        return a
+
+
+def color_revealer(words: List[Word]):
+    before = " ".join([(lambda w: w.before)(w) for w in words])
+    colored_children = [
+        (lambda w: Node(before=Writing(w.before, w.color)))(w) for w in words
+    ]
+
+    return Node(before=Writing(before), children=Children(colored_children, "-"))
+
+    after = " ".join([(lambda w: w.before)(w) for w in words])
+    return external_sandhi(before, after)
 
 
 def external_sandhi_v2(words: List[Word]):
-    def aft(a, b):
-        if a == b:
-            return None
-        else:
-            return a
-
     before = " ".join([(lambda w: w.before)(w) for w in words])
     after = " ".join([(lambda w: w.safe_after())(w) for w in words])
     return external_sandhi(before, after)
@@ -251,10 +272,10 @@ def external_sandhi(before: LiteralString, after: LiteralString):
             return a
 
     return Node(
-        before=before,
+        before=Writing(before),
         children=Children(
             [
-                (lambda b, a: Node(before=b, after=aft(a, b)))(b, a)
+                (lambda b, a: Node(before=Writing(b), after=aft(a, b)))(b, a)
                 for (b, a) in list(zip(before.split(" "), after.split(" ")))
             ],
             "-",
@@ -324,46 +345,52 @@ class SlokaTime(Timeline):
 
         # node = node
 
-        node1 = external_sandhi(
-            "yo mAM pashyati sarvatra sarvaM cha mayi pashyati ।",
-            "yo mAm pashyati sarvatra sarvam cha mayi pashyati",
-        )
-        node2 = external_sandhi(
-            "tasyAhaM na praNashyAmi sa ca me na praNashyati ।।",
-            "tasyAham na praNashyAmi sa ca me na praNashyati",
-        )
+        # node1 = external_sandhi(
+        #     "yo mAM pashyati sarvatra sarvaM cha mayi pashyati ।",
+        #     "yo mAm pashyati sarvatra sarvam cha mayi pashyati",
+        # )
+        # node2 = external_sandhi(
+        #     "tasyAhaM na praNashyAmi sa ca me na praNashyati ।।",
+        #     "tasyAham na praNashyAmi sa ca me na praNashyati",
+        # )
 
-        # words = [
-        #     Word("tasyAhaM", "tasyAham"),
-        #     Word("na"),
-        #     Word("praNashyAmi"),
-        #     Word("sa"),
-        #     Word("ca"),
-        #     Word("me"),
-        #     Word("na"),
-        #     Word("praNashyati"),
-        # ]
-        # node = external_sandhi_v2(words)
+        words = [
+            Word("tasyAhaM", "tasyAham"),
+            Word("na", color=BLUE),
+            Word("praNashyAmi"),
+            Word("sa"),
+            Word("ca"),
+            Word("me"),
+            Word("na"),
+            Word("praNashyati"),
+        ]
+        # Node()
+        # node1 = external_sandhi_v2(words)
+        node1 = color_revealer(words)
 
         # node =
 
         node1.bg.points.move_to(ORIGIN + UP / 2.0)
         print("styles: " + str(node1.bg.get_available_styles()))
         # node1.bg[0].scale_descendants_stroke_radius(2)
-        gold1 = Group(*node1.bg.copy(), color=PURE_GREEN)
+        # gold1 = Group(*node1.bg.copy())
+        # gold1 = Group(*node1.bg.copy(), color=PURE_GREEN)
         # gold1[0].scale_descendants_stroke_radius(2.0)
 
         self.play(
             Aligned(
                 # Write(node1.bg, at=1.0, duration=6.0),
-                Write(gold1, duration=6.0),
+                Write(node1.bg, duration=6.0),
             )
         )
 
         self.play()
 
-        node2.bg.points.move_to(ORIGIN + DOWN / 2.0)
-        gold2 = Group(*node2.bg.copy(), color=PURE_GREEN)
+        node1.deconstruct(self)
+
+        """ node2.bg.points.move_to(ORIGIN + DOWN / 2.0)
+        # gold2 = Group(*node2.bg.copy(), color=PURE_GREEN)
+        gold2 = Group(*node2.bg.copy())
         # gold2[0].scale_descendants_stroke_radius(2.0)
 
         self.play(
@@ -373,7 +400,7 @@ class SlokaTime(Timeline):
             )
         )
 
-        self.play(Wait(duration=10.0))
+        self.play(Wait(duration=10.0)) """
 
         # self.play(Write(node1.bg, duration=6.0))
         # # self.play(Write(node1.bg))
